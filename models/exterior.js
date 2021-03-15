ecd15ee9ed761587816ea45ca0b000063955c40e
czulueta <https://github.com/czulueta/weed.git>
2021-03-14 21:16:05 -0700

    built basic backend

---
+++ models/exterior.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const exteriorSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    small: {
+        type: String,
+    },
+    medium: {
+        type: String,
+    },
+    big: {
+        type: String,
+    },
+})
+module.exports = mongoose.model("Exterior", exteriorSchema)
